e4a3b02179e249d8f348cc53ac81c3e41acddda5
luisescx <luis.esculapio@Live.com>
2022-02-09 22:11:35 -0300

    Fix: added new function on index project

--- project_1/index.js
+++ project_1/index.js
@@ -3,6 +3,33 @@ const fn = require("./functions");
 
 const fullPath = path.join(__dirname, "..", "data", "legends");
 
-fn.readFile(fullPath)
-    .then((files) => fn.elementsFinishedWith(files, "srt"))
+const symbols = [
+    ".",
+    "?",
+    "-",
+    ",",
+    '"',
+    "_",
+    "<i>",
+    "♪",
+    "</i>",
+    "\r",
+    "[",
+    "]",
+    "(",
+    ")",
+];
+
+fn.readFolder(fullPath)
+    .then(fn.elementsFinishedWith(".srt"))
+    .then(fn.readFiles)
+    .then(fn.joinContent)
+    .then(fn.splitBy("\n"))
+    .then(fn.removeEmptyElements)
+    .then(fn.removeIfIncludes("-->"))
+    .then(fn.removeIfNumber)
+    .then(fn.removeCharacters(symbols))
+    .then(fn.joinContent)
+    .then(fn.splitBy(" "))
+    .then(fn.removeEmptyElements)
     .then(console.log);
